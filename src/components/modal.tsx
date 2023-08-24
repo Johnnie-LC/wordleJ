23397f5a5eb98cd59f0c74ea1e77581addede681
Johnnie Lopez <johesloc@gmail.com>
2023-08-24 15:47:08 -0500

    update modal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -11,18 +11,33 @@ interface Props {
 export const ModalP = ({ statusGame, completedWords, solution, showModal, setShowModal }: Props) => {
   // console.log({ statusGame, completedWords, solution })
 
+  const handleCLoseModal = () => { setShowModal(false) }
+
   return (
         <>
         {
           showModal && (
             <div className={styles.modalViewContainer}>
               <div className={styles.modalContainer}>
-                  <h2>You {statusGame === 'won' ? 'won' : 'lost'}</h2>
+                  {/* <h2>You {statusGame === 'won' ? 'won' : 'lost'}</h2> */}
+                  <h2>Estadisticas</h2>
+                  <section className={styles.containesGameCount}>
+                    <div className={styles.gameCount}>
+                      <h3>8</h3>
+                      <span>Juagadas</span>
+                    </div>
+                    <div className={styles.gameCount}>
+                      <h3>2</h3>
+                      <span>Victorias</span>
+                    </div>
+                  </section>
+                  <section>
+                    Siguiente Palabra
+                  </section>
+                  <section>
+                    <button onClick={handleCLoseModal}>Aceptar</button>
+                  </section>
               </div>
-              <button onClick={() => {
-                console.log('cll')
-                setShowModal(false)
-              } }>Aceptar</button>
             </div>
           )
         }
